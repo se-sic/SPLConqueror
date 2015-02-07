1ef0d6f75720f4237ba08ea284f2998519c087fb
nsiegmun <siegmunn@uni-passau.de>
2015-02-07 13:46:54 +0100

    added properties to the VariabilityModel

--- SPLConqueror/SPLConqueror/VariabilityModel.cs
+++ SPLConqueror/SPLConqueror/VariabilityModel.cs
@@ -12,5 +12,40 @@ namespace SPLConqueror_Core
     {
         List<NumericOption> numericOptions = new List<NumericOption>();
 
+        internal List<NumericOption> NumericOptions
+        {
+            get { return numericOptions; }
+            set { numericOptions = value; }
+        }
+
+        List<BinaryOption> binaryOptions = new List<BinaryOption>();
+
+        internal List<BinaryOption> BinaryOptions
+        {
+            get { return binaryOptions; }
+            set { binaryOptions = value; }
+        }
+
+        String name = "empty";
+
+        /// <summary>
+        /// Name of the variability model or configurable program
+        /// </summary>
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        String path = "";
+
+        /// <summary>
+        /// Local path, in which the model is located
+        /// </summary>
+        public String Path
+        {
+            get { return path; }
+            set { path = value; }
+        }
     }
 }
