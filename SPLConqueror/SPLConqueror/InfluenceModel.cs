ff76fd9520d834bf9ccd2f28d707b44973c7594a
nsiegmun <siegmunn@uni-passau.de>
2015-02-07 18:18:13 +0100

    added estimate function and print functionality to the InfluenceModel

--- SPLConqueror/SPLConqueror/InfluenceModel.cs
+++ SPLConqueror/SPLConqueror/InfluenceModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace SPLConqueror_Core
 {
@@ -43,13 +44,46 @@ namespace SPLConqueror_Core
         /// <summary>
         /// Estimates for the given confugration the corresponding value of the non-functional property based on the determined influences of all configuration options.
         /// </summary>
-        /// <param name="c"></param>
+        /// <param name="c">The configuration for which the estimation should be calculated</param>
         /// <returns>Estimated value</returns>
         public double estimate(Configuration c)
         {
             double estimate = 0;
-
+            foreach (BinaryOption binOpt in this.BinaryOptionsInfluence.Keys)
+            {
+                estimate += this.BinaryOptionsInfluence[binOpt].eval(c);
+            }
+            foreach (NumericOption numOpt in this.NumericOptionsInfluence.Keys)
+            {
+                estimate += this.NumericOptionsInfluence[numOpt].eval(c);
+            }
+            foreach (var interaction in this.InteractionInfluence.Keys)
+            {
+                estimate += this.InteractionInfluence[interaction].eval(c);
+            }
             return estimate;
+        }
+
+        /// <summary>
+        /// Prints the influence model as a single function (all influence functions will be concatenated) in a single file
+        /// </summary>
+        /// <param name="file">Path of the file which stores the model</param>
+        public void printModelAsFunction(String file)
+        {
+            StreamWriter sw = new StreamWriter(file);
+            foreach (BinaryOption binOpt in this.BinaryOptionsInfluence.Keys)
+            {
+               sw.WriteLine(this.BinaryOptionsInfluence[binOpt].ToString() + " + ");
+            }
+            foreach (NumericOption numOpt in this.NumericOptionsInfluence.Keys)
+            {
+                sw.WriteLine(this.NumericOptionsInfluence[numOpt].ToString() + " + ");
+            }
+            foreach (var interaction in this.InteractionInfluence.Keys)
+            {
+                sw.WriteLine(this.InteractionInfluence[interaction].ToString() + " + ");
+            }
+            sw.Close();
         }
     }
 }
