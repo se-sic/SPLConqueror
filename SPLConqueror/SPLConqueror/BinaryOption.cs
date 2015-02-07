44ad1a676a7a1722214df881bed77958e4ea436a
AlexanderGrebhahn <alexandergrebhahn@gmail.com>
2015-02-07 14:42:43 +0100

    added methods in Configuration.cs and value structure in BinaryOption.cs + GlobalState.cs

--- SPLConqueror/SPLConqueror/BinaryOption.cs
+++ SPLConqueror/SPLConqueror/BinaryOption.cs
@@ -7,5 +7,17 @@ namespace SPLConqueror_Core
 {
     class BinaryOption : ConfigurationOption
     {
+        /// <summary>
+        /// A binary feature can either be selected or selected in a specific configuration of a programm.
+        /// </summary>
+        public enum Value { Selected, Deselected };
+
+        /// <summary>
+        /// The default value of a binary option.
+        /// </summary>
+        public Value defaultValue { get; set; }
+
+
+
     }
 }
