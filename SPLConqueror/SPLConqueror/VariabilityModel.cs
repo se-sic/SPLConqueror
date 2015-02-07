0da4b1bc4c1debdbd6af335d9c899f83bf4e1330
nsiegmun <siegmunn@uni-passau.de>
2015-02-07 12:22:17 +0100

    Changed Structure

---
+++ SPLConqueror/SPLConqueror/VariabilityModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SPLConqueror_Core
+{
+    /// <summary>
+    /// Central model to store all configuration options and their constraints
+    /// </summary>
+    class VariabilityModel
+    {
+    }
+}
