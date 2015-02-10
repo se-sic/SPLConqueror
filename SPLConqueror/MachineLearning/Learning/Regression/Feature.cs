428625c4ca16ab5d55ac901797c00550db8c22e0
nsiegmun <siegmunn@uni-passau.de>
2015-02-10 17:23:05 +0100

    added Feature class to feature subset selection

---
+++ SPLConqueror/MachineLearning/Learning/Regression/Feature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SPLConqueror_Core;
+
+namespace MachineLearning.Learning.Regression
+{
+    public class Feature : InfluenceFunction
+    {
+        public override bool Equals(object obj)
+        {
+            Feature other = (Feature) obj;
+            return base.Equals(obj);
+        }
+    }
+}
