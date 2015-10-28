d5828a219a4bf06ece26a7527855fadf7db8c39f
kapfe93 <christian_kapfhammer@gmx.de>
2015-10-28 13:51:53 +0100

    Commit for GUI of SPLConqueror, added getExpressionTree() to InfluenceFunction, set CheckConfigSAT to public

---
+++ SPLConqueror/SPLConqueror_GUI/FactorizationSettings.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace SPLConqueror_GUI
+{
+    partial class FactorizationSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.okButton = new System.Windows.Forms.Button();
+            this.settingsPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(177, 325);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 3;
+            this.okButton.Text = "Ok";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // settingsPanel
+            // 
+            this.settingsPanel.AutoScroll = true;
+            this.settingsPanel.Location = new System.Drawing.Point(2, 12);
+            this.settingsPanel.Name = "settingsPanel";
+            this.settingsPanel.Size = new System.Drawing.Size(250, 303);
+            this.settingsPanel.TabIndex = 4;
+            // 
+            // FactorizationSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 360);
+            this.Controls.Add(this.settingsPanel);
+            this.Controls.Add(this.okButton);
+            this.Name = "FactorizationSettings";
+            this.Text = "Factorization Settings";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Panel settingsPanel;
+    }
+}
