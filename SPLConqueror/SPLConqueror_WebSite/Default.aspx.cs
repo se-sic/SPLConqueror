946e4d51eebc5250fc0cffb1904d0675c6b743d7
kapfe93 <christian_kapfhammer@gmx.de>
2016-01-20 11:06:35 +0100

    Designing the site for analyzing the function

--- SPLConqueror/SPLConqueror_WebSite/Default.aspx.cs
+++ SPLConqueror/SPLConqueror_WebSite/Default.aspx.cs
@@ -10,20 +10,9 @@ namespace SPLConqueror_WebSite
 {
     public partial class _Default : Page
     {
-        private const string STATUS_NO_MODEL = "No model loaded.";
-        private const string STATUS_NO_FUNCTION = "Model loaded, no function learned yet.";
-        private const string STATUS_FUNCTION_LEARNED = "Model loaded, function learned.";
-
-        public string learnedFunction = "";
-
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (GlobalState.varModel == null)
-                status.Text = STATUS_NO_MODEL;
-            else if (learnedFunction.Count() == 0)
-                status.Text = STATUS_NO_FUNCTION;
-            else
-                status.Text = STATUS_FUNCTION_LEARNED;
+
         }
     }
 }
