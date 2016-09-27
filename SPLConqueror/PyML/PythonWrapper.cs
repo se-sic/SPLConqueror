d4c0f26b7233511bf4309f86bc44376e0d2e6017
DonHok <basti.fleischmann@gmx.de>
2016-09-27 12:06:59 +0200

    added functionality and communication control

--- SPLConqueror/PyML/PythonWrapper.cs
+++ SPLConqueror/PyML/PythonWrapper.cs
@@ -1,6 +1,7 @@
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using SPLConqueror_Core;
 using System.Collections.Generic;
+using System;
 using System.Text;
 
 namespace ProcessWrapper
@@ -9,9 +10,25 @@ namespace ProcessWrapper
     {
         private Process pythonProcess;
 
-        private const string PYTHON_APP = "Python.exe";
+        private const string PYTHON_APP = "python.exe";
 
-        private const string CONFIG_SEPERATOR = "$";
+        private const string CONFIG_STREAM_START = "config_start";
+
+        private const string CONFIG_STREAM_END = "config_end";
+
+        private const string LEARNING_SETTINGS_STREAM_START = "settings_start";
+
+        private const string LEARNING_SETTINGS_STREAM_END = "settings_end";
+
+        private const string AWAITING_SETTINGS = "req_settings";
+
+        private const string AWAITING_CONFIGS = "req_configs";
+
+        private const string REQUESTING_LEARNING_RESULTS = "req_results";
+
+        private const string PASS_OK = "pass_ok";
+
+        private const string FINISHED_LEARNING = "learn_finished";
 
         /// <summary>
         /// Create a new wrapper that contains a running Python Process.
@@ -28,11 +45,11 @@ namespace ProcessWrapper
             pythonSetup.UseShellExecute = false;
             pythonSetup.RedirectStandardInput = true;
             pythonSetup.RedirectStandardOutput = true;
-            pythonSetup.RedirectStandardError = true;
+        //    pythonSetup.RedirectStandardError = true;
             pythonProcess = Process.Start(pythonSetup);
         }
 
-        public string waitForNextReceivedLine()
+        private string waitForNextReceivedLine()
         {
             string processOutput = "";
             while (processOutput == null || processOutput.Length == 0)
@@ -42,7 +59,7 @@ namespace ProcessWrapper
             return processOutput;
         }
 
-        public void passLineToApplication(string toPass)
+        private void passLineToApplication(string toPass)
         {
             pythonProcess.StandardInput.WriteLine(toPass);
         }
@@ -52,15 +69,80 @@ namespace ProcessWrapper
             pythonProcess.Close();
         }
 
-        public static string formatConfigurationsToString(List<Configuration> toParse)
+        private void passConfigurations(List<Configuration> toPass)
         {
-            StringBuilder sb = new StringBuilder();
-            sb.Append("Configurations" + CONFIG_SEPERATOR);
-            foreach (Configuration config in toParse)
+            passLineToApplication(CONFIG_STREAM_START);
+
+            foreach (Configuration config in toPass)
             {
-                sb.Append(config.ToString() + "&;&" + config.GetNFPValue().ToString("R") + CONFIG_SEPERATOR);
+                passLineToApplication(config.toNumeric());
+                while (!waitForNextReceivedLine().Equals(PASS_OK))
+                {
+                    // Wait to make sure the previous input is processed in order to make sure receiver wont be overwhelmed
+                }
+            }
+
+                passLineToApplication(CONFIG_STREAM_END);
+        }
+
+        private void initializeLearning(LearningSettings.LearningStrategies strategy, LearningSettings.LearningKernel kernelSettings)
+        {
+            passLineToApplication(LEARNING_SETTINGS_STREAM_START);
+            passLineToApplication(Enum.GetName(typeof(LearningSettings.LearningStrategies), (int)strategy));
+            passLineToApplication(Enum.GetName(typeof(LearningSettings.LearningKernel), (int)kernelSettings));
+            passLineToApplication(LEARNING_SETTINGS_STREAM_END);
+        }
+
+        private string formatPyResults(string pythonList)
+        {
+            string withoutBracket = pythonList.Substring(1, pythonList.Length - 2);
+            string[] separators = new String[] { "," };
+            string[] optionPrefix = withoutBracket.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            StringBuilder sb = new StringBuilder();
+            if (optionPrefix.Length == GlobalState.varModel.optionToIndex.Count)
+            {
+                int counter = 0;
+                foreach (KeyValuePair<int, ConfigurationOption> option in GlobalState.varModel.optionToIndex)
+                {
+                    sb.Append(optionPrefix[counter] + " " + option.Value.ToString());
+                    if (counter < optionPrefix.Length - 1)
+                    {
+                        sb.Append(" + ");
+                    }
+                    counter++;
+                }
             }
             return sb.ToString();
+        }
+
+        public void setupApplication(List<Configuration> configs, LearningSettings.LearningStrategies strategy, LearningSettings.LearningKernel kernelSettings)
+        {
+            if (AWAITING_SETTINGS.Equals(waitForNextReceivedLine()))
+            {
+                initializeLearning(strategy, kernelSettings);
+
+                if (AWAITING_CONFIGS.Equals(waitForNextReceivedLine()))
+                {
+                    passConfigurations(configs);
+                }
+            }
+        }
+
+        public void setupDefaultApplication(List<Configuration> configs, LearningSettings.LearningStrategies strategy)
+        {
+            setupApplication(configs, strategy, LearningSettings.LearningKernel.standard);
+        }
+
+        public string getLearningResult()
+        {
+
+            while (!waitForNextReceivedLine().Equals(FINISHED_LEARNING))
+            {
+                
+            }
+
+            passLineToApplication(REQUESTING_LEARNING_RESULTS);
+            return formatPyResults(waitForNextReceivedLine());
         }
     }
 }
