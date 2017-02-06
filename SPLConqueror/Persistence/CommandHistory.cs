33fdcef0d4a89508de3292da4ea4127396124130
DonHok <basti.fleischmann@gmx.de>
2017-02-06 14:47:43 +0100

    Started implementing a simulation of the commands in order to restore the last consistent state from log Files.
    Former-commit-id: 15e1e4fa9adef8d507782880d3c960f2f053738e

---
+++ SPLConqueror/Persistence/CommandHistory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Persistence
+{
+    public class CommandHistory
+    {
+        private Queue<string> commandHistory = new Queue<string>();
+
+        public CommandHistory()
+        {
+
+        }
+
+        public void addCommand(string command)
+        {
+            commandHistory.Enqueue(command);
+            if (commandHistory.Count > 10)
+            {
+                commandHistory.Dequeue();
+            }
+        }
+    }
+}
