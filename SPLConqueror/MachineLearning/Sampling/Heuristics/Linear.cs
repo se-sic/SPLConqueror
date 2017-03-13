ea7d16a367ea33e3afff0b8bb329bf0d56796a5f
agrebhahn <alexander.grebhahn@uni-passau.de>
2017-03-13 10:25:54 +0100

    Added further sampling strategies for binary options.

---
+++ SPLConqueror/MachineLearning/Sampling/Heuristics/Linear.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.SolverFoundation.Solvers;
+using SPLConqueror_Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineLearning.Sampling.Heuristics
+{
+    class Linear
+    {
+
+        private List<List<BinaryOption>> configurations = new List<List<BinaryOption>>();
+        private int counter;
+
+        public const string PARAMETER_NUMCONFIGS_AS_OW = "numOW";
+        public const string PARAMETER_NUMCONFIGS_AS_PW = "numPW";
+
+        public const string PARAMETER_NUMCONFIGS_NAME = "numConfigs";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="vm">The variablity model of the case-study system.</param>
+        /// <param name="treshold">Maximal number of configurations selected by the sampling strategy.</param>
+        /// <param name="timeout">Timeout in seconds.</param>
+        /// <returns></returns>
+        public List<List<BinaryOption>> GenerateRLinear(VariabilityModel vm, int treshold, int timeout)
+        {
+            Solver.VariantGenerator generator = new Solver.VariantGenerator(null);
+
+            List<List<BinaryOption>> erglist = new List<List<BinaryOption>>();
+
+            var tasks = new Task[vm.BinaryOptions.Count];
+            var mylock = new object();
+
+            for (var i = 1; i <= vm.BinaryOptions.Count; i++)
+            {
+                var i1 = i;
+                tasks[i - 1] = Task.Factory.StartNew(() =>
+                {
+                    var size = LinearSize(vm.BinaryOptions.Count, treshold, i1);
+                    return generator.generateTilSize(i1, size, timeout, vm);
+
+                }).ContinueWith(task =>
+                {
+                    if (!task.Status.Equals(TaskStatus.Faulted))
+                    {
+                        lock (mylock)
+                        {
+                            if (task != null) { 
+
+                                erglist.AddRange(task.Result);
+                                counter++;
+                            }
+                        }
+
+                    }
+                   
+                });
+
+            }
+
+            Task.WaitAll(tasks);
+
+            return erglist;
+        }
+
+        public int LinearSize(int numFeatures, int tresh, int index)
+        {
+            int size;
+            float slope = (tresh / (float)numFeatures);
+            if (index < numFeatures / 2.0f)
+            {
+                size = (int)(2 * slope * index) + tresh;
+            }
+            else
+            {
+                size = (int)(-index * 2 * slope) + 3 * tresh;
+            }
+           // Console.WriteLine(size);
+            return size;
+        }
+
+    }
+}
---
+++ SPLConqueror/MachineLearning/Sampling/Heuristics/TWise.cs
@@ -0,0 +1,112 @@
+﻿using SPLConqueror_Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MachineLearning.Sampling.Heuristics
+{
+    class TWise
+    {
+        private Solver.VariantGenerator generator = new Solver.VariantGenerator(null);
+
+        public const string PARAMETER_T_NAME = "t";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="vm">The variability model containing the binary options for which we want to generate the pair-wise configurations.</param>
+        /// <param name="t"> The t of the t-wise</param>
+        /// <returns>A list of configurations in which each configuration is represented by a list of SELECTED binary options</returns>
+        public List<List<BinaryOption>> generateT_WiseVariants_new(VariabilityModel vm, int t)
+        {
+            List<BinaryOption> candidate = new List<BinaryOption>();
+            List<List<BinaryOption>> result = new List<List<BinaryOption>>();
+            generatePowerSet(vm, candidate, t, result, 0);
+
+            return result;
+
+        }
+
+        private List<List<BinaryOption>> generatePowerSet(VariabilityModel vm, List<BinaryOption> candidates, int t, List<List<BinaryOption>> result, int index)
+        {
+            if (candidates.Count == t)
+            {
+                candidates = generator.minimizeConfig(candidates, vm, true, null);
+                result.Add(candidates);
+                return null;
+            }
+
+            for (int i = index; i < GlobalState.varModel.BinaryOptions.Count; i++)
+            {
+                if (candidates.Count < t)
+                {
+                    if (!candidates.Contains(GlobalState.varModel.BinaryOptions[i]))
+                    {
+                        List<BinaryOption> newCand = new List<BinaryOption>();
+                        newCand.AddRange(candidates);
+                        newCand.Add(GlobalState.varModel.BinaryOptions[i]);
+
+                        if (newOptionIsValidForCandidate(candidates, GlobalState.varModel.BinaryOptions[i]))
+                        {
+                            generatePowerSet(vm, newCand, t, result, i + 1);
+
+                            if (candidates.Count == t)
+                            {
+                                candidates = generator.minimizeConfig(candidates, vm, true, null);
+                                result.Add(candidates);
+                                break;
+                            }
+
+                        }
+                    }
+
+                }
+
+            }
+            return null;
+        }
+
+        private bool newOptionIsValidForCandidate(List<BinaryOption> candidates, BinaryOption binaryOption)
+        {
+
+            for (int i = 0; i < candidates.Count; i++)
+            {
+                BinaryOption inList = candidates[i];
+
+                //if (!activeLearning.Contains(pair.Name))
+                //    continue;
+                //Check parent-child relationship
+                if (inList.isAncestor(binaryOption) || binaryOption.isAncestor(inList) || inList == binaryOption)
+                    return false;
+
+                //Check if one option implies the presence of the other option
+                bool impliedOption = false;
+                foreach (var implied in inList.Implied_Options)
+                {
+                    if (implied.Count == 1 && implied[0] == binaryOption)
+                    {
+                        impliedOption = true;
+                        break;
+                    }
+                }
+                if (impliedOption)
+                    return false;
+                //vice versa
+                foreach (var implied in binaryOption.Implied_Options)
+                {
+                    if (implied.Count == 1 && implied[0] == inList)
+                    {
+                        impliedOption = true;
+                        break;
+                    }
+                }
+                if (impliedOption)
+                    return false;
+
+
+            }
+            return true;
+        }
+    }
+}
