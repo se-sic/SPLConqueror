f2d2bb533c747f51c8f83d43eabda39092be1368
DonHok <basti.fleischmann@gmx.de>
2017-07-25 23:02:28 +0200

    Removed endless loop when using random or k-exchange sampling. Added tests for splconqueror core.
    Former-commit-id: f4f3f52a4801bd6a2669bd6e5abfcb691b36580f

---
+++ SPLConqueror/SPLConquerorCoreTest/ConfigurationTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using NUnit.Framework;
+using SPLConqueror_Core;
+using System.Collections.Generic;
+
+namespace SPLConquerorCoreTest
+{
+    [TestFixture]
+    public class ConfigurationTest
+    {
+        [Test]
+        public void TestConfigurationPositiv()
+        {
+            VariabilityModel vm = new VariabilityModel("test");
+            initVM(vm);
+            GlobalState.varModel = vm;
+            Dictionary<NFProperty, double> measuredValuesFirst = new Dictionary<NFProperty, double>();
+            measuredValuesFirst.Add(GlobalState.getOrCreateProperty("test"), 2);
+            Dictionary<NFProperty, double> measuredValuesSecond = new Dictionary<NFProperty, double>();
+            measuredValuesSecond.Add(GlobalState.getOrCreateProperty("test"), 3);
+            Dictionary<BinaryOption, BinaryOption.BinaryValue> binaryOptionsFirst = 
+                new Dictionary<BinaryOption, BinaryOption.BinaryValue>();
+            fillUpBinaryOptions(binaryOptionsFirst, vm);
+            Dictionary<BinaryOption, BinaryOption.BinaryValue> binaryOptionsSecond =
+                new Dictionary<BinaryOption, BinaryOption.BinaryValue>();
+            fillUpBinaryOptions(binaryOptionsSecond, vm);
+            Dictionary<NumericOption, double> numericOptionsFirst =
+                new Dictionary<NumericOption, double>();
+            fillUpNumericOptions(numericOptionsFirst, vm);
+            Dictionary<NumericOption, double> numericOptionsSecond =
+                new Dictionary<NumericOption, double>();
+            fillUpNumericOptions(numericOptionsSecond, vm);
+            Configuration configFirst = new Configuration(binaryOptionsFirst, 
+                numericOptionsFirst, measuredValuesFirst);
+            Configuration configSecond = new Configuration(binaryOptionsSecond,
+                numericOptionsSecond, measuredValuesSecond);
+            Assert.AreEqual(configFirst, configSecond);
+        }
+
+        [Test]
+        public void TestConfigurationNegativ()
+        {
+            VariabilityModel vm = new VariabilityModel("test");
+            initVM(vm);
+            GlobalState.varModel = vm;
+            Dictionary<BinaryOption, BinaryOption.BinaryValue> binaryOptionsFirst = 
+                new Dictionary<BinaryOption, BinaryOption.BinaryValue>();
+            fillUpBinaryOptions(binaryOptionsFirst, vm);
+            Dictionary<NumericOption, double> numericOptionsFirst =
+                new Dictionary<NumericOption, double>();
+            fillUpNumericOptions(numericOptionsFirst, vm);
+            Configuration firstConfig = new Configuration(binaryOptionsFirst, numericOptionsFirst);
+            firstConfig.update();
+            Configuration secondConfig = new Configuration(new Dictionary<BinaryOption, BinaryOption.BinaryValue>()
+                , new Dictionary<NumericOption, double>());
+            Assert.AreNotEqual(firstConfig.ToString(), secondConfig.ToString());
+        }
+
+        private static void fillUpBinaryOptions(Dictionary<BinaryOption, BinaryOption.BinaryValue> toFill,
+            VariabilityModel vm)
+        {
+            toFill.Add(new BinaryOption(vm, "testOption1"), BinaryOption.BinaryValue.Selected);
+            toFill.Add(new BinaryOption(vm, "testOption2"), BinaryOption.BinaryValue.Deselected);
+        }
+
+        private static void fillUpNumericOptions(Dictionary<NumericOption, double> toFill,
+            VariabilityModel vm)
+        {
+            toFill.Add(new NumericOption(vm, "testOption3"), 3);
+            toFill.Add(new NumericOption(vm, "testOption4"), 4);
+        }
+
+        private static void initVM(VariabilityModel vm)
+        {
+            vm.addConfigurationOption(new BinaryOption(vm, "testOption1"));
+            vm.addConfigurationOption(new BinaryOption(vm, "testOption2"));
+            vm.addConfigurationOption(new NumericOption(vm, "testOption3"));
+            vm.addConfigurationOption(new NumericOption(vm, "testOption4"));
+        }
+    }
+}
---
+++ SPLConqueror/SPLConquerorCoreTest/GlobalStateTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+using SPLConqueror_Core;
+using System.Collections.Generic;
+
+namespace SPLConquerorCoreTest
+{
+    [TestFixture]
+    public class GlobalStateTest
+    {
+        [Test, Order(1)]
+        public void TestNFProperty()
+        {
+            NFProperty created = GlobalState.getOrCreateProperty("testProperty");
+            Assert.AreEqual(created, GlobalState.getOrCreateProperty(created.Name));
+            GlobalState.setDefaultProperty(created.Name);
+            Assert.AreEqual(created, GlobalState.currentNFP);
+        }
+
+        [Test, Order(2)]
+        public void TestMeasurements()
+        {
+            GlobalState.varModel = new VariabilityModel("testModel");
+            Dictionary<NFProperty, double> measuredValues = new Dictionary<NFProperty, double>();
+            measuredValues.Add(GlobalState.currentNFP, 2);
+            Configuration toAdd = new Configuration(new Dictionary<BinaryOption, BinaryOption.BinaryValue>(),
+                new Dictionary<NumericOption, double>(), measuredValues);
+            GlobalState.addConfiguration(toAdd);
+            List<Configuration> measured = new List<Configuration>();
+            measured.Add(toAdd);
+            Assert.AreEqual(toAdd.GetNFPValue(), GlobalState.getMeasuredConfigs(measured).ToArray()[0].GetNFPValue());
+        }
+
+        [Test, Order(3)]
+        public void TestClearGlobalState()
+        {
+            GlobalState.clear();
+            Assert.IsNull(GlobalState.varModel);
+            Assert.IsNull(GlobalState.currentNFP);
+            Assert.AreEqual(0, GlobalState.allMeasurements.Configurations.Count);
+            Assert.AreEqual(0, GlobalState.evaluationSet.Configurations.Count);
+            Assert.AreEqual(0, GlobalState.nfProperties.Count);
+            Assert.AreEqual(0, GlobalState.optionOrder.Count);
+        }
+    }
+}
