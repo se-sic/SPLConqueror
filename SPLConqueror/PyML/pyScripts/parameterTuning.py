c82907148db24526f09f883e213032bbd6210ce7
AlexanderGrebhahn <alexandergrebhahn@gmail.com>
2017-10-16 07:52:48 +0200

    change parameter space

--- SPLConqueror/PyML/pyScripts/parameterTuning.py
+++ SPLConqueror/PyML/pyScripts/parameterTuning.py
@@ -3,15 +3,14 @@ import sklearn.ensemble as skEn
 import sklearn.neighbors as skNE
 import sklearn.kernel_ridge as skKR
 import sklearn.tree as skTr
-import sklearn.grid_search as modelSel
+import sklearn.model_selection as modelSel
 
 import numpy as np
 
 # default parameter spaces used for parameter tuning.
 
 param_SVR = [
-    {'C': [0.5, 1, 2, 5, 10], 'epsilon': [0.3, 0.2, 0.1, 0.01], 'kernel': ['rbf', 'linear', 'poly', 'sigmoid'],
-     'degree': [1, 2, 3, 4], 'coef0': [0, 1, 2], 'shrinking': [True, False], 'tol': [5e-3, 1e-2, 2e-2, 5e-2, 1e-1]}
+    {'C': [0.5, 1, 2, 5], 'epsilon': [0.3, 0.2, 0.1], 'kernel': ['rbf', 'poly'],'degree': [1, 2, 3], 'coef0': [0, 1, 2], 'shrinking': [True, False], 'tol': [5e-2, 1e-1, 2e-1, 5e-1]}
 ]
 
 param_DecisionTree = [
@@ -25,12 +24,12 @@ param_RandomForest = [
 
 param_kNNRegressor = [
     {'n_neighbors': [8, 9, 10, 11, 12, 13, 14, 15], 'weights': ['uniform', 'distance'], 'algorithm': ['auto'],
-     'p': [1, 2, 3], 'n_jobs': [-1]}
+     'p': [1, 2, 3]}
 ]
 
 param_kernelRidge = [
-    {'alpha': [1e0, 0.1, 1e-2, 1e-3, 1e-4, 1e-5], 'kernel': ['rbf', 'linear', 'poly', 'sigmoid'],
-     'degree': [1, 2, 3, 4, 5], 'gamma': [0.01, 0.05, 0.1, 0.2, 0.3]}
+    {'alpha': [0.1, 1e-2, 1e-3, 1e-4], 'kernel': ['rbf', 'poly'],
+     'degree': [1, 2, 3], 'gamma': [0.01, 0.05, 0.1, 0.2]}
 ]
 
 param_baggingSVR = [
@@ -81,35 +80,40 @@ def scoreFunction(estimator, configurations, measurements):
     sum = 0.0
     for i in range(len(measurements)):
         sum = np.abs(measurements[i] - predictions[i]) / measurements[i]
+    f = open('H:\ExpDesignPaper\DefaultParameters\jobs\javaGC_Whole_default_python\py.log', 'a+')
+    f.write(str(sum))
+    f.write('\n')
+    f.close()
     return sum
 
 
 def optimize_SVR(X_train, y_train):
-    opt = modelSel.GridSearchCV(sk.SVR(cache_size=500), param_SVR, cv=5, scoring=scoreFunction)
+    opt = modelSel.GridSearchCV(estimator = sk.SVR(cache_size = 1000), param_grid = param_SVR, cv=5 , scoring =scoreFunction, n_jobs = 1, pre_dispatch = '2*n_jobs')
     opt.fit(X_train, y_train)
+
     return formatOptimal(opt.best_params_)
 
 
 def optimize_DecisionTree(X_train, y_train):
-    opt = modelSel.GridSearchCV(skTr.DecisionTreeRegressor(cache_size=500), param_DecisionTree, cv=5, scoring=scoreFunction)
+    opt = modelSel.GridSearchCV(skTr.DecisionTreeRegressor(), param_DecisionTree, cv=5, scoring=scoreFunction)
     opt.fit(X_train, y_train)
     return formatOptimal(opt.best_params_)
 
 
 def optimize_RandomForestRegressor(X_train, y_train):
-    opt = modelSel.GridSearchCV(skEn.RandomForestRegressor(cache_size=500), param_RandomForest, cv=5, scoring=scoreFunction)
+    opt = modelSel.GridSearchCV(skEn.RandomForestRegressor(), param_RandomForest, cv=5, scoring=scoreFunction)
     opt.fit(X_train, y_train)
     return formatOptimal(opt.best_params_)
 
 
 def optimize_KNNeighborsRegressor(X_train, y_train):
-    opt = modelSel.GridSearchCV(skNE.KNeighborsRegressor(cache_size=500), param_kNNRegressor, cv=5, scoring=scoreFunction)
+    opt = modelSel.GridSearchCV(skNE.KNeighborsRegressor(), param_kNNRegressor, cv=5, scoring=scoreFunction)
     opt.fit(X_train, y_train)
     return formatOptimal(opt.best_params_)
 
 
 def optimize_KernelRidge(X_train, y_train):
-    opt = modelSel.GridSearchCV(skKR.KernelRidge(cache_size=500), param_kernelRidge, cv=5, scoring=scoreFunction)
+    opt = modelSel.GridSearchCV(skKR.KernelRidge(), param_kernelRidge, cv=5, scoring=scoreFunction)
     opt.fit(X_train, y_train)
     return formatOptimal(opt.best_params_)
 
@@ -124,6 +128,11 @@ def optimize_BaggingSVR(X_train, y_train):
 
 # Format the best configuration found during parameter tuning.
 def formatOptimal(optimalParams):
+    f = open('H:\ExpDesignPaper\DefaultParameters\jobs\javaGC_Whole_default_python\py.log', 'a+')
+    f.write(str(optimalParams))
+    f.write('\n')
+    f.close()
+
     if len(optimalParams.keys()) == 0:
         return "No parameter has an influence on the accuracy of the result!"
     output = ""
