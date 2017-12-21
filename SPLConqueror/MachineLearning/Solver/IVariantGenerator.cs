ac3e2b77b383d40c2ea057c3b66c2f473d357057
Christian Kaltenecker <kaltenec@fim.uni-passau.de>
2017-12-21 16:41:33 +0100

    Added the z3 solver library partially. For now, it is possible to retrieve all valid configurations from a feature model and t-wise sampling.
    Former-commit-id: 8d2e019f22bd00bde92a76a828b3400d0528c4b0

--- SPLConqueror/MachineLearning/Solver/IVariantGenerator.cs
+++ SPLConqueror/MachineLearning/Solver/IVariantGenerator.cs
@@ -14,9 +14,19 @@ namespace MachineLearning.Solver
         /// </summary>
         /// <param name="vm">The variability model containing the binary options and their constraints.</param>
         /// <returns>Returns a list of configurations, in which a configuration is a list of SELECTED binary options (deselected options are not present)</returns>
-        List<List<BinaryOption>> generateAllVariantsFast(VariabilityModel vm);
-        
-        
+        List<List<BinaryOption>> GenerateAllVariantsFast(VariabilityModel vm);
+
+        /// <summary>
+        /// Generates up to n solutions of the given variability model. 
+        /// Note that this method could also generate less than n solutions if the variability model does not contain sufficient solutions.
+        /// Moreover, in the case that <code>n &lt; 0</code>, all solutions are generated.
+        /// </summary>
+        /// <param name="vm">The <see cref="VariabilityModel"/> to obtain solutions for.</param>
+        /// <param name="n">The number of solutions to obtain.</param>
+        /// <returns>A list of configurations, in which a configuration is a list of SELECTED binary options.</returns>
+        List<List<BinaryOption>> GenerateUpToNFast(VariabilityModel vm, int n);
+
+
         /// <summary>
         /// Simulates a simple method to get valid configurations of binary options of a variability model. The randomness is simulated by the modulu value.
         /// We take only the modulu'th configuration into the result set based on the CSP solvers output.
@@ -25,7 +35,7 @@ namespace MachineLearning.Solver
         /// <param name="treshold">Maximum number of configurations</param>
         /// <param name="modulu">Each configuration that is % modulu == 0 is taken to the result set</param>
         /// <returns>Returns a list of configurations, in which a configuration is a list of SELECTED binary options (deselected options are not present</returns>
-        List<List<BinaryOption>> generateRandomVariants(VariabilityModel vm, int treshold, int modulu);
+        List<List<BinaryOption>> GenerateRandomVariants(VariabilityModel vm, int treshold, int modulu);
         
 
         //Configuration size
@@ -37,7 +47,7 @@ namespace MachineLearning.Solver
         /// <param name="minimize">If true, we search for the smallest (in terms of selected options) valid configuration. If false, we search for the largest one.</param>
         /// <param name="unWantedOptions">Binary options that we do not want to become part of the configuration. Might be part if there is no other valid configuration without them.</param>
         /// <returns>The valid configuration (or null if there is none) that satisfies the VM and the goal.</returns>
-        List<BinaryOption> minimizeConfig(List<BinaryOption> config, VariabilityModel vm, bool minimize, List<BinaryOption> unWantedOptions);
+        List<BinaryOption> MinimizeConfig(List<BinaryOption> config, VariabilityModel vm, bool minimize, List<BinaryOption> unWantedOptions);
 
         /// <summary>
         /// Based on a given (partial) configuration and a variability, we aim at finding all optimally maximal or minimal (in terms of selected binary options) configurations.
@@ -47,7 +57,7 @@ namespace MachineLearning.Solver
         /// <param name="minimize">If true, we search for the smallest (in terms of selected options) valid configuration. If false, we search for the largest one.</param>
         /// <param name="unwantedOptions">Binary options that we do not want to become part of the configuration. Might be part if there is no other valid configuration without them</param>
         /// <returns>A list of configurations that satisfies the VM and the goal (or null if there is none).</returns>
-        List<List<BinaryOption>> maximizeConfig(List<BinaryOption> config, VariabilityModel vm, bool minimize, List<BinaryOption> unwantedOptions);
+        List<List<BinaryOption>> MaximizeConfig(List<BinaryOption> config, VariabilityModel vm, bool minimize, List<BinaryOption> unwantedOptions);
 
         /// <summary>
         /// The method aims at finding a configuration which is similar to the given configuration, but does not contain the optionToBeRemoved. If further options need to be removed from the given configuration, they are outputed in removedElements.
@@ -57,26 +67,16 @@ namespace MachineLearning.Solver
         /// <param name="removedElements">If further options need to be removed from the given configuration to build a valid configuration, they are outputed in this list.</param>
         /// <param name="vm">The variability model containing all options and their constraints.</param>
         /// <returns>A configuration that is valid, similar to the original configuration and does not contain the optionToBeRemoved.</returns>
-        List<BinaryOption> generateConfigWithoutOption(BinaryOption optionToBeRemoved, List<BinaryOption> originalConfig, out List<BinaryOption> removedElements, VariabilityModel vm);
+        List<BinaryOption> GenerateConfigWithoutOption(BinaryOption optionToBeRemoved, List<BinaryOption> originalConfig, out List<BinaryOption> removedElements, VariabilityModel vm);
 
-
-        List<List<BinaryOption>> generateTilSize(int i1, int size, int timeout, VariabilityModel vm);
-
-        //Old: Refactoring of VM
-        //Determine the boundaries in terms of cardinality of a product at which (a) valid products can be generated (maxBoundary) and (b) the first invalid products can be generated (minBoundary)
-        //int determineBoundary(VariabilityModel vm, RuntimeProperty rp, NFPConstraint constraint, bool min, bool withInteractions, bool invalidBoundary);//searching for invalid or valid products?
-        //Computes Configurations that have a given number of features: it tries to find at least a configuration for each feature or #repetitions and at maximum a configuration for each feature or #repetitions
-        //List<List<BinaryOption>> getConfigWithNbOfFeatures(int nbOfFeatures, VariabilityModel vm, bool withInteractions, int repetitions, List<BinaryOption> forbiddenOptions);
-
-        //Old: Refactoring of VM
-        //List<Element> getInvalidVariantAtCardinality(int i, NFPConstraint constraint, FeatureModel fm, RuntimeProperty rp, Dictionary<int, List<List<Element>>> combiNotToBeSelected, bool findInvalid);
-        //List<List<Element>> getAllConfigsForCardinality(int nbOfFeatures, FeatureModel fm, bool withDerivatives);
-        //List<Element> determineSetOfValidNegativeFeatures(int nbOfFeatures, FeatureModel fm, bool withDerivatives, List<Element> forbiddenFeatures, RuntimeProperty rp, NFPConstraint constraint);
-
-
-
-        //Old:
-        //For feature uncertainty
-        //List<Configuration> getMinimalConfigurationCSP(FeatureModel fm, List<Configuration> unWantedConfigurations, Element selectedFeature, int minsize);
+        /// <summary>
+        /// This method returns a configuration, which is minimal according to the given featureweight.
+        /// </summary>
+        /// <param name="vm">The variability model containing all options and their constraints.</param>
+        /// <param name="numberSelectedFeatures">The number of features that should be selected.</param>
+        /// <param name="featureWeight">The weight of every feature.</param>
+        /// <param name="lastSampledConfiguration">The set containing all already sampled configurations.</param>
+        /// <returns>A list of <see cref="BinaryOption"/>, which should be selected.</returns>
+        List<BinaryOption> WeightMinimization(VariabilityModel vm, int numberSelectedFeatures, Dictionary<BinaryOption, int> featureWeight, List<Configuration> lastSampledConfigurations);
     }
 }
