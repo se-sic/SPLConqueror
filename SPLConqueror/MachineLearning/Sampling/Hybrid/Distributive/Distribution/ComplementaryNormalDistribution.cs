4f7a01c87102bf927811d9d217997ce230dfa1a0
Christian Kaltenecker <kaltenec@fim.uni-passau.de>
2018-06-27 09:21:56 +0200

    Implemented different distributions.

---
+++ SPLConqueror/MachineLearning/Sampling/Hybrid/Distributive/Distribution/ComplementaryNormalDistribution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MachineLearning.Sampling.Hybrid.Distributive.Distribution
+{
+    /// <summary>
+    /// This class represents the complementary normal distribution.
+    /// </summary>
+    public class ComplementaryNormalDistribution : NormalDistribution
+    {
+
+        /// <summary>
+        /// Creates the distribution by using the given mean and deviation value.
+        /// </summary>
+        /// <param name="allBuckets">All buckets.</param>
+        /// <param name="mean">The mean for the normal distribution.</param>
+        /// <param name="deviation">The standard deviation for the normal distribution.</param>
+        public Dictionary<double, double> CreateDistribution(List<double> allBuckets, double mean, double deviation)
+        {
+            Dictionary<double, double> result = base.CreateDistribution(allBuckets, mean, deviation);
+
+            // Now, use the complement
+            foreach (double key in result.Keys) {
+                result[key] = 1 - result[key];
+            }
+
+            result = DistributionUtils.AdjustToOne(result);
+
+            return result;
+        }
+
+        /// <summary>
+        /// See <see cref="Distribution.GetName"/>.
+        /// </summary>
+        public string GetName()
+        {
+            return "COMPLEMENTARYNORMAL";
+        }
+        
+    }
+}
---
+++ SPLConqueror/MachineLearning/Sampling/Hybrid/Distributive/Distribution/DistributionUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MachineLearning.Sampling.Hybrid.Distributive
+{
+    /// <summary>
+    /// This class includes some utilities that are suitable for distributions.
+    /// </summary>
+    public class DistributionUtils
+    {
+
+        /// <summary>
+        /// This method adjusts the given distribution so that the sum is 1.0.
+        /// </summary>
+        /// <returns>The adjusted distribution.</returns>
+        /// <param name="distribution">The distribution to adjust.</param>
+        public static Dictionary<double, double> AdjustToOne(Dictionary<double, double> distribution) {
+            Dictionary<double, double> newDistribution = new Dictionary<double, double>();
+
+            double sum = 0;
+            foreach(double key in distribution.Keys) {
+                sum += distribution[key];
+            }
+
+            foreach(double key in distribution.Keys) {
+                newDistribution[key] = distribution[key] / sum;
+            }
+
+            return newDistribution;
+        }
+    }
+}
