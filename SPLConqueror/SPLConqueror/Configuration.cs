b70e89ed2ca3ec1c89ec700f3b00d251c9e88856
Christian Kaltenecker <kaltenec@fim.uni-passau.de>
2018-10-02 09:08:05 +0200

    Fixed a bug that occured when printing configurations without any NFP.

--- SPLConqueror/SPLConqueror/Configuration.cs
+++ SPLConqueror/SPLConqueror/Configuration.cs
@@ -685,7 +685,7 @@ namespace SPLConqueror_Core
 
             }
 
-	     if (nfpProperties.Count != 0 && !GlobalState.currentNFP.Equals(NFProperty.DefaultProperty))
+	     if (nfpProperties != null && nfpProperties.Count != 0 && !GlobalState.currentNFP.Equals(NFProperty.DefaultProperty))
             {
                 result.Append(ConfigurationPrinter.CSV_ELEMENT_DELIMITER);
 				foreach (NFProperty nfp in this.nfpValues.Keys) {
